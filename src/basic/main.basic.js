994699bfb706ea20663d8f08c3863ce31969b3d6
Suin Kim <contact@suinkim.me>
2025-04-15 10:49:45 +0900

    chore: setTimeout 주석 처리

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -61,32 +61,32 @@ function main() {
 
   root.appendChild(cont);
   calcCart();
-  setTimeout(function () {
-    setInterval(function () {
-      var luckyItem = products[Math.floor(Math.random() * products.length)];
-      if (Math.random() < 0.3 && luckyItem.q > 0) {
-        luckyItem.val = Math.round(luckyItem.val * 0.8);
-        alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
-        updateSelOpts();
-      }
-    }, 30000);
-  }, Math.random() * 10000);
-  setTimeout(function () {
-    setInterval(function () {
-      if (lastSel) {
-        var suggest = products.find(function (item) {
-          return item.id !== lastSel && item.q > 0;
-        });
-        if (suggest) {
-          alert(
-            suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!'
-          );
-          suggest.val = Math.round(suggest.val * 0.95);
-          updateSelOpts();
-        }
-      }
-    }, 60000);
-  }, Math.random() * 20000);
+  // setTimeout(function () {
+  //   setInterval(function () {
+  //     var luckyItem = products[Math.floor(Math.random() * products.length)];
+  //     if (Math.random() < 0.3 && luckyItem.q > 0) {
+  //       luckyItem.val = Math.round(luckyItem.val * 0.8);
+  //       alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
+  //       updateSelOpts();
+  //     }
+  //   }, 30000);
+  // }, Math.random() * 10000);
+  // setTimeout(function () {
+  //   setInterval(function () {
+  //     if (lastSel) {
+  //       var suggest = products.find(function (item) {
+  //         return item.id !== lastSel && item.q > 0;
+  //       });
+  //       if (suggest) {
+  //         alert(
+  //           suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!'
+  //         );
+  //         suggest.val = Math.round(suggest.val * 0.95);
+  //         updateSelOpts();
+  //       }
+  //     }
+  //   }, 60000);
+  // }, Math.random() * 20000);
 }
 
 /**
