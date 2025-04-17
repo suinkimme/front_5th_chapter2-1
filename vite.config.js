65d616ee269361e97d21b5b06bd916af68ce513b
Suin Kim <contact@suinkim.me>
2025-04-17 16:57:56 +0900

    chore: 배포에 필요한 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
-  },
-})
+export default mergeConfig(
+  defineConfig({
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          basic: resolve(__dirname, 'index.basic.html'),
+          advanced: resolve(__dirname, 'index.advanced.html'),
+        },
+      },
+    },
+    base:
+      process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '/',
+    plugins: [react()],
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/setupTests.js',
+    },
+  })
+);
