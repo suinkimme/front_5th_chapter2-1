46c651421302f8484213030d6c2a3ac27e3d6833
Suin Kim <contact@suinkim.me>
2025-04-17 16:58:07 +0900

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // 여기에 custom 규칙 추가
+  },
+};
